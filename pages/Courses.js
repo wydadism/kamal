fc1c08d5e6ea1e2d5dd92320c9023bce146a3ac5
wydadism <94148495+wydadism@users.noreply.github.com>
2021-11-12 01:44:19 +0100

    Update Courses.js

--- pages/Courses.js
+++ pages/Courses.js
@@ -1,7 +1,7 @@
 import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
-import Section from '../components/section'
-import { GridItem } from '../components/grid-item'
+
+
 
 
 
